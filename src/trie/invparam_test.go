9540c3071cea20714a282348c4f8732a4c22fd88
Aleks Obukhov <a.a.obukhov@gmail.com>
2021-08-14 12:25:24 +0200

    Add test for unmarshalling

--- src/trie/invparam_test.go
+++ src/trie/invparam_test.go
@@ -1,6 +1,7 @@
 package trie
 
 import (
+	"encoding/json"
 	"github.com/stretchr/testify/suite"
 	"testing"
 )
@@ -14,6 +15,25 @@ func (suite *InvParamTestSuite) TestStringPanic() {
 		p := InvParam(9999)
 		_ = p.String()
 	})
+}
+
+func (suite *InvParamTestSuite) TestUnmarshalText() {
+	jsonString := `["BytesSize", "KeysCount"]`
+	var result []InvParam
+
+	err := json.Unmarshal([]byte(jsonString), &result)
+
+	suite.Assert().Nil(err)
+	suite.Assert().Equal([]InvParam{BytesSize, KeysCount}, result)
+}
+
+func (suite *InvParamTestSuite) TestUnmarshalTextError() {
+	jsonString := `["BytesSizeFoo", "KeysCount"]`
+	var result []InvParam
+
+	err := json.Unmarshal([]byte(jsonString), &result)
+
+	suite.Assert().NotNil(err)
 }
 
 func TestInvParamTestSuite(t *testing.T) {
